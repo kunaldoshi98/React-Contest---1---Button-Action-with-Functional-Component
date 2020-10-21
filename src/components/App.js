1a60a7ad6cadb0fa00bb24e177c175c7ffa39602
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-10-21 22:01:09 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,19 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  function handleclick() {}
+  function handleclick() {
+    var id = document.getElementById("para");
+    id.hidden = false;
+  }
   return (
     <div id="main">
       <button id="click" onClick={handleclick}>
         Click
       </button>
-      <p id="para">
+      <p id="para" hidden>
         Hello, I've learnt to use the full-stack evaluation tool. This makes me
         so happy
       </p>
-      ;
     </div>
   );
 }
