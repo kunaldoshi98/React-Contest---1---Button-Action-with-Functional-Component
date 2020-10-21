38d18ea5f1cef67f5c9d85be2132a7b6a2bababc
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-10-21 21:55:20 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,17 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  var state = false;
-  function handleclick() {
-    state = true;
-  }
+  function handleclick() {}
   return (
     <div id="main">
       <button id="click" onClick={handleclick}>
         Click
       </button>
-      {state && (
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes
-          me so happy
-        </p>
-      )}
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      ;
     </div>
   );
 }
