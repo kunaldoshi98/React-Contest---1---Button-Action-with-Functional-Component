f2092b4d499b13d7e04e218ccdcf5ffd47dd9698
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-10-21 21:35:31 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick="handleClick()">
+        Click
+      </button>
     </div>
   );
 }
-
 
 export default App;
