1c1ca03dd00b311f694964cc8c13960e8b3b9787
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-11-26 15:17:59 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,33 @@
 import React, { Component, useState } from "react";
-import "./../styles/App.css";
+import "../styles/App.css";
 
-function App() {
-  return (
-    <div id="main">
-      <button
-        id="click"
-        onClick={() => {
-          let para = document.createElement("P");
-          para.innerText =
-            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-          para.id = "para";
-          document.body.appendChild(para);
-          let click = document.getElementById("click");
-          click.disabled = true;
-        }}
-      >
-        Click
-      </button>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      render: false
+    };
+    this.handleclick = this.handleclick.bind(this);
+  }
+  handleclick() {
+    this.setState({ render: true });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.handleclick}>
+          Click
+        </button>
+        {this.state.render && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
